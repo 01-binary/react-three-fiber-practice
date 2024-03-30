127e88799b8265d45e168b32e29bf769d41ae656
jinsoo <binary01@kakao.com>
2024-03-30 20:03:28 +0900

    feat: controls, helper add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Canvas } from '@react-three/fiber';
 import ThreeElement from './ThreeElement';
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
   return (
@@ -7,12 +8,19 @@ function App() {
       <Canvas
         // orthographic
         camera={{
-          near: 1,
-          far: 20,
+          // near: 1,
+          // far: 20,
           fov: 75,
           position: [0, 5, 5],
         }}
       >
+        {/* Math.PI = 180 */}
+        <OrbitControls
+          minAzimuthAngle={-Math.PI / 4}
+          maxAzimuthAngle={Math.PI / 4}
+        />
+        <axesHelper args={[6]} />
+        <gridHelper args={[10, 10, 'pink', 'black']} />
         <ThreeElement />
       </Canvas>
     </>
