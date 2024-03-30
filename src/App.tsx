c7d16c88c0ccae02212506df217b1857832e5d7f
jinsoo <binary01@kakao.com>
2024-03-30 20:20:20 +0900

    feat: leva

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { Canvas } from '@react-three/fiber';
 import ThreeElement from './ThreeElement';
 import { OrbitControls } from '@react-three/drei';
+import { useControls } from 'leva';
 
 function App() {
+  const color = useControls({
+    value: 'green',
+  });
+
+  const grid = useControls({
+    segment: { value: 10, min: 1, max: 100, step: 1 },
+  });
+
   return (
     <>
       <Canvas
@@ -15,12 +24,13 @@ function App() {
         }}
       >
         {/* Math.PI = 180 */}
+        <color attach="background" args={[color.value]} />
         <OrbitControls
           minAzimuthAngle={-Math.PI / 4}
           maxAzimuthAngle={Math.PI / 4}
         />
         <axesHelper args={[6]} />
-        <gridHelper args={[10, 10, 'pink', 'black']} />
+        <gridHelper args={[10, grid.segment, 'pink', 'black']} />
         <ThreeElement />
       </Canvas>
     </>
--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,16 +1,20 @@
 import { useThree, useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
 import * as Three from 'three';
+import { useControls } from 'leva';
 
 const ThreeElement = () => {
   const boxRef = useRef<Three.Mesh>(null);
   const { size, gl, scene, camera } = useThree();
 
+  const box = useControls({
+    rotation: { value: 10, min: -360, max: 360, step: 1 },
+  });
+
   useFrame((state, delta) => {
-    boxRef.current!.rotation.x += delta;
+    // boxRef.current!.rotation.x += delta;
     // boxRef.current!.position.y -= 0.01;
     // boxRef.current!.scale.z += 0.01;
-
     // console.log(boxRef.current);
     // console.log(state, delta);
   });
@@ -24,12 +28,12 @@ const ThreeElement = () => {
         ref={boxRef}
         rotation={[
           Three.MathUtils.degToRad(45),
-          Three.MathUtils.degToRad(45),
+          Three.MathUtils.degToRad(box.rotation),
           0,
         ]}
       >
         <boxGeometry />
-        <sphereGeometry />
+        {/* <sphereGeometry /> */}
         <meshStandardMaterial color={'red'} />
       </mesh>
     </>
